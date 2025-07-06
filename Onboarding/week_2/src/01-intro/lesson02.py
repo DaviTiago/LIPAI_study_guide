fe632267a7ec2733a3b216608fc3b6becdd39188
Davi Tiago <davi.tiago@ufu.br>
2025-07-06 12:44:16 -0300

    Keywords and identifiers

---
+++ Onboarding/week_2/src/01-intro/lesson02.py
@@ -0,0 +1,30 @@
+"""Indentifiers"""
+
+# Identifiers are names used to identify variables, functions, classes, modules, and other objects in Python.
+# They must follow certain rules:
+# 1. Must start with a letter (a-z, A-Z) or an underscore (_).
+# reserved words (keywords) cannot be used as identifiers:
+# True, False, None, and import, keywords like if, else, for, while, def, class, etc.
+
+name = 'Maria'
+age = 30
+Name = 'João'
+complet_name = 'Maria da Silva'
+
+#good practice: (clean code)
+c = 10
+count = 10
+
+s= 10 + 20
+sum
+
+#Constants
+PI = 3.14
+
+age = 18
+adult = 18
+
+if age >= adult:
+    print('You are an adult.')
+else:
+    print('You are a minor.')
