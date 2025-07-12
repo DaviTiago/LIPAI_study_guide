2dc7cc53298ca6c3f42219ff96090454bfcff26b
Davi Tiago <davi.tiago@ufu.br>
2025-07-12 17:49:27 -0300

    Exercise 01

--- Onboarding/week_2/src/03-data-types/exercises/ex01.py
+++ Onboarding/week_2/src/03-data-types/exercises/ex01.py
@@ -14,11 +14,20 @@ while len(numbers) < 3:
     else:
         print("Por favor, digite um número válido.")
 
-minor = min(numbers)
-maior = max(numbers)
+min_number = numbers[0]
+max_number = numbers[0]
+
+for num in numbers:
+    if num < min_number:
+        min_number = num
+    if num > max_number:
+        max_number = num
+#podemos usar min() e max() para simplificar
+# min_number = min(numbers)
+# max_number = max(numbers)
 
 print(numbers)
-print(f"Menor: {minor}")
-print(f"Maior: {maior}")
+print(f"Menor: {min_number}")
+print(f"Maior: {max_number}")
 
 
---
+++ Onboarding/week_2/src/03-data-types/exercises/ex02.py
@@ -0,0 +1,5 @@
+"""Exercise 02"""
+
+# - Solicite ao usuário o mês do ano no formato numérico 1, 2, 3 ..12 e apresente o nome do ano.
+# - Exemplo: entrada 3 saída ‘Março’.
+# - **Implementar com Tupla**
