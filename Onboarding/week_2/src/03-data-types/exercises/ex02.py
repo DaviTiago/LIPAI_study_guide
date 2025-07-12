5c2bdcb03375bac384fa3191d0dbf168bab60b6c
Davi Tiago <davi.tiago@ufu.br>
2025-07-12 18:07:02 -0300

    Exercise 02

--- Onboarding/week_2/src/03-data-types/exercises/ex02.py
+++ Onboarding/week_2/src/03-data-types/exercises/ex02.py
@@ -3,3 +3,21 @@
 # - Solicite ao usuário o mês do ano no formato numérico 1, 2, 3 ..12 e apresente o nome do ano.
 # - Exemplo: entrada 3 saída ‘Março’.
 # - **Implementar com Tupla**
+
+months = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+          "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+
+while True:
+    try:
+        n_month = int(input('Digite o número do mês (1-12):'))
+        if 1 <= n_month <= 12:
+            print(f'O mês é: {months[n_month-1]}')
+            break
+        else:
+            print('Digite um número válido entre 1 a 12')
+    except ValueError:
+        print('Entrada inválida. Digite um número inteiro')
+
+
+        
+
